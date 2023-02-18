7263e9aca4e089615de8f01ade6b7428f27abf13
rainqiu <530004000@qq.com>
2023-02-18 19:25:00 +0800

    设置配置文件

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/qiuyuhome/gin-demo/pkg/setting"
+
+var (
+	ServerSetting    *setting.ServerSettingS
+	AppSetting       *setting.AppSettingS
+	DatabaseSettring *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/qiuyuhome/gin-demo/global"
 	"github.com/qiuyuhome/gin-demo/internal/routers"
+	"github.com/qiuyuhome/gin-demo/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
+func setupSetting() error {
+	s, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = s.ReadSection("Database", &global.DatabaseSettring)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouters()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 		// ReadHeaderTimeout: 0,
 		// TLSConfig:         nil,
