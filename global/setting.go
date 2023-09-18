44862126a284891e7dbc9a062299d21aa5e095cb
rainqiu <yu.qiu@antiratech.com.cn>
2023-09-18 09:12:18 +0800

    数据库连接

--- global/setting.go
+++
@@ -1,9 +0,0 @@
-package global
-
-import "github.com/qiuyuhome/gin-demo/pkg/setting"
-
-var (
-	ServerSetting    *setting.ServerSettingS
-	AppSetting       *setting.AppSettingS
-	DatabaseSettring *setting.DatabaseSettingS
-)
--- main.go
+++ main.go
@@ -1,72 +1,18 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/qiuyuhome/gin-demo/global"
-	"github.com/qiuyuhome/gin-demo/internal/model"
-	"github.com/qiuyuhome/gin-demo/internal/routers"
-	"github.com/qiuyuhome/gin-demo/pkg/setting"
-	"log"
-	"net/http"
-	"time"
-)
-
-func init() {
-	err := setupSetting()
-	if err != nil {
-		log.Fatalf("init.setupSetting err: %v", err)
-	}
-}
-
-func setupSetting() error {
-	s, err := setting.NewSetting()
-	if err != nil {
-		return err
-	}
-	err = s.ReadSection("Server", &global.ServerSetting)
-	if err != nil {
-		return err
-	}
-	err = s.ReadSection("App", &global.AppSetting)
-	if err != nil {
-		return err
-	}
-	err = s.ReadSection("Database", &global.DatabaseSettring)
-	if err != nil {
-		return err
-	}
-	global.ServerSetting.ReadTimeout *= time.Second
-	global.ServerSetting.WriteTimeout *= time.Second
-	return nil
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
-	gin.SetMode(global.ServerSetting.RunMode)
-	router := routers.NewRouters()
-	s := &http.Server{
-		Addr:           ":" + global.ServerSetting.HttpPort,
-		Handler:        router,
-		ReadTimeout:    global.ServerSetting.ReadTimeout,
-		WriteTimeout:   global.ServerSetting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
-		// ReadHeaderTimeout: 0,
-		// TLSConfig:         nil,
-		// IdleTimeout:       0,
-		// TLSNextProto:      nil,
-		// ConnState:         nil,
-		// ErrorLog:          nil,
-		// BaseContext:       nil,
-		// ConnContext:       nil,
-	}
-
-	s.ListenAndServe()
-}
-
-func setupDBEngine() error {
-	var err error
-	global.DBEngine, err = model.NewDBEngine(global.DatabaseSettring)
-	if err != nil {
-		return err
-	}
-	return nil
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
